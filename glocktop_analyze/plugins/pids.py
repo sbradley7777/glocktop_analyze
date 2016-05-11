8cb407eafdcdc8f43d6cbce3b890fd56cb851a96
Shane Bradley <sbradley@redhat.com>
2016-05-11 11:01:44 -0400

    [plugins/pids.py] Updated the output that is generated for text/console output.
    Signed-off-by: Shane Bradley <sbradley@redhat.com>

--- glocktop_analyze/plugins/pids.py
+++ glocktop_analyze/plugins/pids.py
@@ -63,6 +63,7 @@ class Pids(Plugin):
     def __get_text(self, colorize=False):
         summary = ""
         if (self.__pids_in_snapshots):
+            summary += "\nThe number of times a pid appeared in a snapshot.\n"
             summary += "%s\n\n" %(tableize(self.__pids_in_snapshots,
                                            ["Filesystem", "Pid", "Command",
                                             "Number of Snapshots Appeared in"], colorize).strip())
@@ -71,6 +72,7 @@ class Pids(Plugin):
             ftable = []
             for row in self.__pids_using_multiple_glocks:
                 ftable += tableify(row)
+            summary += "The glocks the pids appears in queue as holder or waiter\n"
             summary += "%s\n\n" %(tableize(ftable,
                                            ["Filesystem", "Pid", "Command",
                                             "Number of Glocks Appeared in",
@@ -115,13 +117,13 @@ class Pids(Plugin):
     def console(self):
         summary = self.__get_text(colorize=True)
         if (summary):
-            print "%s: %s\n%s\n" %(self.get_title(), self.get_description(), summary.strip())
+            print "%s: %s\n\n%s\n" %(self.get_title(), self.get_description(), summary.strip())
 
     def write(self, html_format=False):
         if (not html_format):
             wdata = self.__get_text(colorize=False)
             if (wdata):
-                wdata = "%s: %s\n%s\n" %(self.get_title(), self.get_description(), wdata.strip())
+                wdata = "%s: %s\n\n%s\n" %(self.get_title(), self.get_description(), wdata.strip())
                 filename = "%s.txt" %(self.get_title().lower().replace(" - ", "-").replace(" ", "_"))
                 path_to_output_file = os.path.join(os.path.join(self.get_path_to_output_dir(),
                                                                 self.get_filesystem_name()), filename)
